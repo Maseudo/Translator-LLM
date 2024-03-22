76f6917ee9b59ac15508937fb42f9ad1608f57e8
Maseudo <46875906+Maseudo@users.noreply.github.com>
2024-03-22 13:04:21 -0500

    Attempt to get model split across gpu

--- Translator.py
+++ Translator.py
@@ -1,18 +1,23 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, pipeline, Conversation, TextIteratorStreamer
 from threading import Thread
+from accelerate import infer_auto_device_map, dispatch_model
 from sys import stdout
+import torch
 import time, os
 from json import load as jload
 
 config = jload(open("config.json"))
 model_config = AutoConfig.from_pretrained(pretrained_model_name_or_path=config['pretrained_model_name_or_path'])
-model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path=config['pretrained_model_name_or_path'], config=model_config, device_map=config['device'])
+model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path=config['pretrained_model_name_or_path'], config=model_config, device_map=config['device'], attn_implementation="flash_attention_2", torch_dtype=torch.float16)
 tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=config['pretrained_model_name_or_path'], config=model_config)
-pipel = pipeline(task=config['task'], model=model, config=model_config, tokenizer=tokenizer, framework=config['framework'], device_map=config['device'], num_workers=8, batch_size=8)
 streamer = TextIteratorStreamer(tokenizer, skip_prompt=True)
+gen_params = {'max_memory': {0: config[maxGPUMemory], 1: config[maxGPUMemory], 2: config[maxGPUMemory], 3: config[maxGPUMemory], 4: config[maxGPUMemory]}, 'eos_token_id': tokenizer.eos_token_id, 'pad_token_id': tokenizer.pad_token_id, 'max_new_tokens': config['max_new_tokens'], 'do_sample': config['do_sample'], 'streamer': streamer, 'temperature': config['temperature'], 'top_k': config['top_k']}
+device_map = infer_auto_device_map(model, max_memory=gen_params['max_memory'], no_split_module_classes=model._no_split_modules, dtype=torch.float16)
+model = dispatch_model(model, device_map)
+pipel = pipeline(task=config['task'], model=model, config=model_config, tokenizer=tokenizer, framework=config['framework'], device_map=device_map)
 
 class Jade:
-	def __init__(self, config, model_config, model, tokenizer, pipel, streamer):
+	def __init__(self):
 		self.config = config
 		self.model_config = model_config
 		self.model = model
@@ -29,7 +34,8 @@ def generate(llm):
 	llm.gen_proc.start()
  
 def generate_do(llm):
-                output = llm.pipeline(llm.content, eos_token_id=llm.tokenizer.eos_token_id, pad_token_id=llm.tokenizer.pad_token_id, max_new_tokens=llm.config['max_new_tokens'], do_sample=llm.config['do_sample'], streamer=llm.streamer, temperature=llm.config['temperature'], top_k=llm.config['top_k'])
+                #output = llm.pipeline(llm.content, eos_token_id=llm.tokenizer.eos_token_id, pad_token_id=llm.tokenizer.pad_token_id, max_new_tokens=llm.config['max_new_tokens'], do_sample=llm.config['do_sample'], streamer=llm.streamer, temperature=llm.config['temperature'], top_k=llm.config['top_k'])
+	output = llm.pipeline(llm.content, **gen_params)
 
 def handle_stream(llm):
 	while llm.stop_tag != True:
@@ -51,7 +57,7 @@ def clr_screen(): os.system('cls')
 
 def reload(): return Jade()
 
-llm = Jade(config, model_config, model, tokenizer, pipel, streamer)
+llm = Jade()
 
 while True:
 	time.sleep(0.2)
